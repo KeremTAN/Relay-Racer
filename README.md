8bc44ad04ac0a63fe54ef19201d0a349fabf6c91
KeremTAN <keremtan.95@hotmail.com>
2023-09-25 06:23:58 +0300

    InputReader is fixed and Readme is written

---
+++ README.md
@@ -0,0 +1,21 @@
+# Relay Racer
+
+### Requirements For This Project
+* CMake
+* Make
+* VsCode
+* Vs Studio [If your operating system is Windows, otherwise you do not need]
+
+### How to Run This Project
+
+* Open the project with VsCode
+
+* Compile & Build the project by pressing the key combination <b><i>ctrl+shift+b </b></i> or <b><i>command+shift+b</b></i>
+
+* If your operating system is Linux or MacOs, you can run the application by clicking  <b><i>Run->Start Debugging</b></i> in <b>VsCode</b>
+
+* If your operating system is Windows, the project may have been compiled to work with <b>Visual Studio</b>
+    * Go to the  <b><i>build directory</i></b> which has created when you compiled the project in VsCode
+    * Open  <b><i>Relay.sln</i></b> file with  <b>Visual Studio</b>
+    * Build the Relay project in the <b><i>Solution Explorer </b></i> section
+    * Finally, you can run the project by right clicking on the Relay project in the Solution Explorer section and selecting  <b><i>Debug->Start New Instance </b></i>
--- include/InputReader.hpp
+++ include/InputReader.hpp
@@ -9,7 +9,7 @@ public:
     InputReader() = default;
     ~InputReader() = default;
 
-    inline bool selectPrintableTeams(const int& argc,char** argv, std::vector<int>& printableTeams) {
+    inline bool selectPrintableTeams(const int& argc, char** argv, std::vector<int>& printableTeams) {
 
         int teamId{};
 
@@ -17,17 +17,24 @@ public:
             std::cout << "Please, enter integers between 1 and 400. Enter the 0 to end the entry process." << std::endl;
 
             while (true) {
-                std::string inputs;
-                std::cout << "Please, enter input: ";
-                std::cin >> inputs;
+                std::string input{};
 
-                teamId = std::atoi(inputs.c_str());
+                try {
+                    std::cout << "Please, enter an integer: ";
+                    std::cin >> input;
+
+                    teamId = std::stoi(input);
+                }
+                catch(const std::exception& e) {
+                    std::cerr << e.what() << '\n';
+                    printCharacterError(input);
+                    return 1;
+                }
 
                 if (teamId == 0) { break; }
                 
                 else if (teamId < 1 || teamId > 400) {
-                    std::cerr << "[Invalid Input] : " << teamId << " is not a number between 1 and 400." << std::endl;
-                    return 1;
+                    printRangeError(teamId);
                 }
 
                 else { printableTeams.push_back(--teamId); }
@@ -41,21 +48,34 @@ public:
 
         else {
             for (int i = 1; i < argc; ++i) {
-                teamId = std::atoi(argv[i]);
+                try {
+                   teamId = std::atoi(argv[i]);
+                }
+                catch(const std::exception& e) {
+                    std::cerr << e.what() << '\n';
+                    return 1;
+                }
 
                 if (teamId == 0 && argv[i][0] != '0') {
-                    std::cerr << "[Invalid Input] : " << argv[i] << " is not a number." << std::endl;
+                    printCharacterError(argv[i]);
                     return 1;
                 }
 
                 if (teamId < 1 || teamId > 400) {
-                    std::cerr << "[Invalid Input] : " << argv[i] << " is not a number between 1 and 400." << std::endl;
+                    printRangeError(teamId);
                     return 1;
                 }
                 printableTeams.push_back(--teamId);
             }
         }
         return 0;
+    }
+private:
+    inline void printRangeError(const int& number){
+        std::cerr << "[Invalid Input] : " << number << " is not a number between 1 and 400." << std::endl;
+    }
+    inline void printCharacterError(const std::string& characters) {
+        std::cerr << "[Invalid Input] : " << characters << " is not a number." << std::endl;
     }
 };
 
